ca103857a8ca4535e0f2fcb278ae5a56d437e7d0
Sabine <sabine.sun@hotmail.com>
2023-03-28 13:30:30 +0200

    style: remove empty line

--- pages/components/snake/Snake.tsx
+++ pages/components/snake/Snake.tsx
@@ -2,26 +2,11 @@ import React from "react";
 
 interface Snake {
   context: CanvasRenderingContext2D;
-  /* XHead: number;
-  YHead: number;
-  XBody: number;
-  YBody: number;*/
   snakeBodyX: number[];
   snakeBodyY: number[];
 }
 
 const pixel = 10;
-/*
-const Snake = ({ context, XHead, YHead, XBody, YBody }: Snake) => {
-  context.fillStyle = "#ffffff"; // Set fill color to red
-  context.strokeStyle = "#010101";
-
-  context.fillRect(XHead, YHead, pixel, pixel);
-  context.strokeRect(XHead, YHead, pixel, pixel);
-  context.fillRect(XBody, YBody, pixel, pixel);
-  context.strokeRect(XBody, YBody, pixel, pixel);
-};*/
-
 const Snake = ({ context, snakeBodyX, snakeBodyY }: Snake) => {
   context.fillStyle = "#ffffff"; // Set fill color to red
   context.strokeStyle = "#010101";
